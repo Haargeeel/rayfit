c48c495688c64dd9a86a800e8b73cabd4805268b
ray <ray.g@gmx.de>
2017-07-31 01:03:32 +0200

    add hubble telescope

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 window.onload = function() {
   const orbitISS = 382.5
+  const orbitHubble = 595
+
   const distanceDiv = document.getElementById('distance')
   const me = document.getElementById('me')
+  const iss = document.getElementById('iss')
+  const hubble = document.getElementById('hubble')
+
   const distanceWidth = distanceDiv.getBoundingClientRect().width
-  me.style.left = `${myDistance * distanceWidth / orbitISS}px`
+
+  me.style.left = `${myDistance * distanceWidth / orbitHubble}px`
   me.style.display = 'block'
+
+  iss.style.left = `${orbitISS * distanceWidth / orbitHubble}px`
+  iss.style.display = 'block'
+
+  hubble.style.right = '0px'
+  hubble.style.display = 'block'
 }
