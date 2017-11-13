fcfc3bccd23b5d524f7248f5a9226134af697f62
ray <ray.g@gmx.de>
2017-11-14 00:30:56 +0100

    rotate it

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 window.onload = function() {
   const orbitISS = 382.5
   const orbitHubble = 595
+  const maxDist = 600
 
   const distanceDiv = document.getElementById('distance')
   const me = document.getElementById('me')
   const iss = document.getElementById('iss')
+  const issWrapper = document.getElementById('issWrapper')
+  const hubbleWrapper = document.getElementById('hubbleWrapper')
   const hubble = document.getElementById('hubble')
 
   const distanceWidth = distanceDiv.getBoundingClientRect().width
 
-  me.style.left = `${myDistance * distanceWidth / orbitHubble}px`
+  me.style.left = `${myDistance * distanceWidth / maxDist}px`
   me.style.display = 'block'
 
-  iss.style.left = `${orbitISS * distanceWidth / orbitHubble}px`
+  issWrapper.style.width = `${2 * orbitISS * distanceWidth / maxDist + 600}px`
+  issWrapper.style.height = `${2 * orbitISS * distanceWidth / maxDist + 600}px`
+  // iss.style.left = `${orbitISS * distanceWidth / orbitHubble + 200}px`
   iss.style.display = 'block'
 
-  hubble.style.right = '0px'
+  hubbleWrapper.style.width = `${2 * orbitHubble * distanceWidth / maxDist + 600}px`
+  hubbleWrapper.style.height = `${2 * orbitHubble * distanceWidth / maxDist + 600}px`
+  // hubble.style.left = `${orbitHubble * distanceWidth / maxDist}px`
   hubble.style.display = 'block'
 }
