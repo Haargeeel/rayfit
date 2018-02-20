861e7198a7caf43228e61bde5bf0d4877dc0aee7
Ray Gläske <ray@Rays-MBP-2.fritz.box>
2018-02-20 22:10:28 +0100

    rescue astronaut from outer space

--- js/script.js
+++ js/script.js
@@ -6,10 +6,18 @@ window.onload = function() {
   const me = document.getElementById('me')
   const iss = document.getElementById('iss')
   const hubble = document.getElementById('hubble')
+  const astronaut = me.getElementsByTagName('img')[0]
 
   const distanceWidth = distanceDiv.getBoundingClientRect().width
 
-  me.style.left = `${myDistance * distanceWidth / orbitHubble}px`
+  const rounds = Math.floor(myDistance / orbitISS)
+  const currentDistance = myDistance % orbitISS
+
+  console.log('rounds', rounds)
+  console.log('distance', currentDistance)
+  astronaut.className = rounds % 2 !== 0 ? 'flip' : ''
+
+  me.style.left = `${currentDistance * distanceWidth / orbitHubble}px`
   me.style.display = 'block'
 
   iss.style.left = `${orbitISS * distanceWidth / orbitHubble}px`
